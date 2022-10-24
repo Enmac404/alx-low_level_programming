b33f3b141f76c000d4ede4c3f436424f9beb2a2e
Olla Emmanuel <ollaemmanuel02@gmail.com>
2022-10-24 05:12:19 -0700

    commit

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,7 +2,10 @@
 /**
  * _strlen - return the length of a string
  * Return: 0 is success
+ * @s: char to check
+ * Description: this will return the length of a string
  */
+
 int _strlen(char *)
 {
 	int a = 0;
